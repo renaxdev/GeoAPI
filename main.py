0171ebe649e26298638f14c1e6acf0291766d9c5
Renax Development <72433516+renaxdev@users.noreply.github.com>
2021-08-26 12:03:00 +0200

    test for heroku

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-from typing import Optional
-
 from fastapi import FastAPI
 
 app = FastAPI()
 
+"""
+GET = Returning
+POST = Sending to endpoint
+PUT = update existing smth
+DELETE = delete smth
+
+"""
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+def home():
+    return {"Data": "Test"}
