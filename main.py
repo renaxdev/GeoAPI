7f99a3ced6720555cbfbc6a02f3830ac00f1b5c3
Renax Development <72433516+renaxdev@users.noreply.github.com>
2021-08-26 17:00:03 +0200

    openapi desc

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 from fastapi import FastAPI
+from fastapi.openapi.utils import get_openapi
 
 app = FastAPI()
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="Stonks API",
+        version="0.0.0 beta",
+        description="An API for different Stock data",
+        routes=app.routes,
+    )
+    openapi_schema["info"]["x-logo"] = {
+        "url": "https://cdn.discordapp.com/attachments/879671944637722625/880421415789162516/Neues_Projekt1.png"
+    }
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+app.openapi = custom_openapi
 
 """
 GET = Returning
