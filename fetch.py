42f642a9355ee796fb14b47c0ffa5da686006f00
JuDev <jahidudd65@gmail.com>
2021-08-31 17:20:44 +0200

    Fetching
    Die Area und die Einwohnerzahl sind nicht Live und besitzen kein tag.

--- fetch.py
+++ fetch.py
@@ -18,9 +18,50 @@ def get_lang(url):
     lang = classes[1].find("a").text.strip()
     return lang
 
+
 def get_president(url):
     site = requests.get(url)
     soup = BeautifulSoup(site.content, "html.parser")
     classes = soup.find_all("td", class_="infobox-data")
-    president = classes[1].find("a").text.strip()
+    president = classes[4].find("a").text.strip()
     return president
+
+
+def get_citizen(url):
+    """
+    Es gibt keine Live Zahlen.
+    Nur Stände von Jahren.
+    Z.B Stand 2020: 83.656....mio Einwohner
+    """
+    site = requests.get(url)
+    soup = BeautifulSoup(site.content, "html.parser")
+    classes = soup.find_all("td", class_="infobox-data")
+    citizen = classes[3].find("img").text.strip()
+    return citizen
+
+
+def get_area():
+    """
+    Das selbe Problem
+    """
+    site = requests.get("https://en.wikipedia.org/wiki/List_of_countries_and_dependencies_by_area")
+    soup = BeautifulSoup(site.content, "html.parser")
+    classes = soup.find_all("td", class_="infobox-data")
+    area = classes[0].find("a").text.strip()
+    return area
+
+
+def get_longitude(url):
+    site = requests.get(url)
+    soup = BeautifulSoup(site.content, "html.parser")
+    classes = soup.find_all("span", class_="longitude")
+    longitude = classes[0].text.strip()
+    return longitude
+
+
+def get_latitude(url):
+    site = requests.get(url)
+    soup = BeautifulSoup(site.content, "html.parser")
+    classes = soup.find_all("span", class_="latitude")
+    latitude = classes[0].text.strip()
+    return latitude
